dd8c28a7e13bedee77a805b03802083377745b7e
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-28 14:58:12 +0600

    Get alluser data from Db

--- index.js
+++ index.js
@@ -50,12 +50,19 @@ async function run() {
         res.send(result);
     });
 
-      //ORDER COLLECTION API
+      //ORDER COLLECTION API | post data from client
        app.post('/order', async(req,res) =>{
         const order = req.body;
         const result = await orderCollection.insertOne(order);
         res.send(result);
-       })  
+       }) 
+      //Get data from Db of client checkout data  
+       app.get('/order', async (req,res) =>{
+        const query = {};
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.send(orders);
+       })
        
 
        
