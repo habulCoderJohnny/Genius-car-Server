b42c5959b80ec01901f9e2facd9d0e3248c50974
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-28 20:24:23 +0600

    Created JWT Token for user

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 //const packageName = require('packageName');
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const app = express();
 require('dotenv').config();
@@ -15,59 +16,71 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try{
+    try {
         await client.connect();
         const serviceCollection = client.db("GeniusCar").collection("Service");
         const orderCollection = client.db("GeniusCar").collection("order");
+        
+        //AUTH related
+        //json web token Issue
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
-       app.get('/service', async(req,res)=>{
-        const query = {};
-        const cursor =serviceCollection.find(query);
-        const services = await cursor.toArray();
-        res.send(services);
-       });
 
-     //FIND ONE (id) 
-       app.get('/service/:id', async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id: ObjectId(id)};
-        const service = await serviceCollection.findOne(query);
-        res.send(service);
-       });
+        //Services API
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
 
-       //POST/ INSERT ONE (form) 
-       app.post('/service', async(req,res)=>{
-        const newService = req.body;
-        const result = await serviceCollection.insertOne(newService);
-        res.send(result);
-       });
+        //FIND ONE (id) 
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
 
-       //DELETE a USER
-       app.delete('/service/:id',async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id: ObjectId(id)};
-        const result = await userCollection.deleteOne(query);
-        res.send(result);
-    });
+        //POST/ INSERT ONE (form) 
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
 
-      //ORDER COLLECTION API | post data from client
-       app.post('/order', async(req,res) =>{
-        const order = req.body;
-        const result = await orderCollection.insertOne(order);
-        res.send(result);
-       }) 
-      //Get individual user data from Db of client Order data based on email 
-       app.get('/order', async (req,res) =>{
-        const email = req.query.email;
-        //console.log(email);
-        const query = {email: email};
-        const cursor = orderCollection.find(query);
-        const orders = await cursor.toArray();
-        res.send(orders);
-       })
-       
+        //DELETE a USER
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        //ORDER COLLECTION API | post data from client
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+        //Get individual user data from Db of client Order data based on email 
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            //console.log(email);
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
@@ -76,10 +89,10 @@ run().catch(console.dir);
 console.log('GENIUS DB now CONNECTED');
 
 //log 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Running genius server...');
 });
 //log 
-app.listen(port,()=>{
-    console.log('johnny Listening.....port', port );
+app.listen(port, () => {
+    console.log('johnny Listening.....port', port);
 })
