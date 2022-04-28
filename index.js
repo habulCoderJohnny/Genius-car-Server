98f2dea0ff78bed30959a111da62b38a7823bb07
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-28 14:33:09 +0600

    create orderCollection & get-order-data from clint-side & create-api

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-// const packageName = require('packageName');
+//const packageName = require('packageName');
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const { ObjectId } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const port = process.env.PORT || 5000;
-
 const app = express();
+require('dotenv').config();
+
 //middleware
 app.use(cors());
 app.use(express.json());
@@ -19,6 +18,7 @@ async function run() {
     try{
         await client.connect();
         const serviceCollection = client.db("GeniusCar").collection("Service");
+        const orderCollection = client.db("GeniusCar").collection("order");
 
        app.get('/service', async(req,res)=>{
         const query = {};
@@ -38,7 +38,7 @@ async function run() {
        //POST/ INSERT ONE (form) 
        app.post('/service', async(req,res)=>{
         const newService = req.body;
-        const result = serviceCollection.insertOne(newService);
+        const result = await serviceCollection.insertOne(newService);
         res.send(result);
        });
 
@@ -49,6 +49,15 @@ async function run() {
         const result = await userCollection.deleteOne(query);
         res.send(result);
     });
+
+      //ORDER COLLECTION API
+       app.post('/order', async(req,res) =>{
+        const order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.send(result);
+       })  
+       
+
        
     }
     finally{
