d6738307fd06d5de34daf9ddf3d54d47bedc7c78
habulCoderJohnny <fokruljohnny@gmail.com>
2022-04-29 22:13:37 +0600

    code correction

--- index.js
+++ index.js
@@ -25,10 +25,9 @@ function verifyJWT(req,res,next) {
         }
         console.log('decoded:', decoded);
         req.decoded = decoded;
+        next();
     })
-    // console.log('inside verifyJWT:', authHeader);
-    next();
-    
+    // console.log('inside verifyJWT:', authHeader);    
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oxx0c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -89,6 +88,8 @@ async function run() {
             res.send(result);
         })
         //Get individual user data from Db of client Order data based on email 
+        //& Decoded , verifyJWT
+    
         app.get('/order', verifyJWT, async (req, res) => {
             const decodedEmail = req.decoded.email;
             const email = req.query.email;
